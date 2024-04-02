75f40b0d195f32f994708d00c61ae590a00355bf
= <mikael.colliander@sj.se>
2024-04-02 11:51:24 +0200

    Added vite-plugin-cjs-interop

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,20 @@ import {telefunc} from "telefunc/vite";
 import ssr from "vike/plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { cjsInterop } from "vite-plugin-cjs-interop";
 
 export default defineConfig({
   plugins: [react({}), ssr({
     prerender: true,
-  }), telefunc(), vercel()],
+  }), telefunc(), vercel(),
+  cjsInterop({
+    dependencies: [
+      // Add problematic npm package here
+      "@mui/material/**",
+    ]
+  })
+],
+ssr: {
+  noExternal: true
+}
 });
